a9ee244648d201f754790f4adf70a865586ff98d
yanknvim <yanknvim@gmail.com>
2023-07-28 09:52:28 +0900

    Add scheme impl

--- scheme/main.scm
+++ scheme/main.scm
@@ -1,9 +1,19 @@
 (define program (read))
 
+(define (atom? x)
+	(and (not (pair? x)) (not (null? x))))
+
 (define (eval s)
-	(display s)
 	(cond
-		((atom s) s)
-		((eq (car s) +) )
+		((atom? s) s)
+		((eq? (car s) '+) (+ (eval (car (cdr s))) (eval (car (cdr (cdr s))))))
+		((eq? (car s) '-) (- (eval (car (cdr s))) (eval (car (cdr (cdr s))))))
+		((eq? (car s) '*) (* (eval (car (cdr s))) (eval (car (cdr (cdr s))))))
+		((eq? (car s) '/) (/ (eval (car (cdr s))) (eval (car (cdr (cdr s))))))
+		((eq? (car s) 'atom?) (atom? (car (cdr s))))
+		((eq? (car s) 'cons) (cons (car (cdr s)) (car (cdr (cdr s)))))
 		)
 	)
+
+(display (eval program))
+(newline)
